a944d7457b0d479e12f515c2463ed6fadd6fc34f
jelenamarjanovic <jelena.marjanovic.13@gmail.com>
2023-02-08 02:03:08 +0100

    Shuffling a deck

--- cards/deck.go
+++ cards/deck.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"math/rand"
 	"os"
 	"strings"
 )
@@ -62,4 +63,12 @@ func newDeckFromFile(filename string) deck {
 	}
 
 	return deck(strings.Split(string(bs), ","))
+}
+
+func (d deck) shuffle() {
+	for i := range d {
+		newPosition := rand.Intn(len(d) - 1)
+
+		d[i], d[newPosition] = d[newPosition], d[i]
+	}
 }
--- cards/main.go
+++ cards/main.go
@@ -1,10 +1,13 @@
 package main
 
 func main() {
-	// cards := newDeck()
+	cards := newDeck()
 
 	// cards.saveToFile("my_cards")
 
-	cards := newDeckFromFile("./my_cards")
+	// cards := newDeckFromFile("./my_cards")
+
+	cards.shuffle()
+
 	cards.print()
 }
