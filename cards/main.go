29d0da0fd2a2f2d587c395530cba81decdb06ae2
jelenamarjanovic <jelena.marjanovic.13@gmail.com>
2023-02-11 05:17:49 +0100

    Create a simple flow

--- cards/main.go
+++ cards/main.go
@@ -1,13 +1,29 @@
 package main
 
+import "fmt"
+
 func main() {
 	cards := newDeck()
 
-	// cards.saveToFile("my_cards")
+	cards.print()
 
-	// cards := newDeckFromFile("./my_cards")
+	cards.saveToFile("my_cards")
+
+	cards = newDeckFromFile("./my_cards")
+
+	fmt.Println("\nShuffle...")
 
 	cards.shuffle()
 
 	cards.print()
+
+	cards.deal(5)
+
+	hand, remainingDeck := cards.deal(5)
+
+	fmt.Println("\nDealing...\n-------------\nHand:")
+	hand.print()
+
+	fmt.Println("\nRemaining deck:")
+	remainingDeck.print()
 }
